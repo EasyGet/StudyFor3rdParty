ebea89b3ac13db0260b70e63772d08bf70e9e419
EasyGet <371814174@qq.com>
2016-04-08 13:54:16 +0800

    Add Project
    Add Project

---
+++ AFNetworkingStudy/AFNetworkingStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkingStudy
+//
+//  Created by Richie on 16/4/8.
+//  Copyright © 2016年 YJZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkingStudy/AFNetworkingStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingStudy
+//
+//  Created by Richie on 16/4/8.
+//  Copyright © 2016年 YJZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
